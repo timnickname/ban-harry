375dbd2053d58b0949402b94cf5ecb152d207297
legofreund2@web.de <timnickname>
2020-12-08 15:02:06 +0100

    fix typo

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ for (var i = 0; i < elements.length; i++) {
         if (node.nodeType === 3) {
             var text = node.nodeValue;
 
-              var replacedText = text.replace('Harry Potter', 'Loser');
+              var replacedText = text.replace('Harry Potter', 'The Loser');
               if (replacedText !== text) {
                   element.replaceChild(document.createTextNode(replacedText), node);
 
